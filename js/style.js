8a03813c3e7e4e3024afde0dd924bc028aeef7ed
sowmik-sec <sowmik.sec@gmail.com>
2024-06-11 17:07:01 +0600

    Styling Dom Properties, Add And Remove Css Class Using JS

---
+++ js/style.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll("section");
+// console.log(sections);
+for (const section of sections) {
+  //   console.log(section);
+  section.style.border = "1px solid grey";
+  section.style.padding = "10px";
+  section.style.marginBottom = "10px";
+  section.style.borderRadius = "10px";
+}
+
+const placesContainer = document.getElementById("places");
+console.log(placesContainer);
+placesContainer.classList.add("yellow-bg");
+
+const placess = document.getElementsByClassName("place");
+for (const place of placess) {
+  place.classList.remove("place");
+}
